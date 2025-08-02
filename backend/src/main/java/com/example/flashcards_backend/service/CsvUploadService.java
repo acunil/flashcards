c6ddf79af7f7e9126721fcc513c96a3e7971a584
Acunil <locamos@gmail.com>
2025-08-02 20:17:03 +0100

    FC-28 Adds unit tests for CsvUploadController

--- backend/src/main/java/com/example/flashcards_backend/service/CsvUploadService.java
+++ backend/src/main/java/com/example/flashcards_backend/service/CsvUploadService.java
@@ -2,8 +2,9 @@ package com.example.flashcards_backend.service;
 
 import com.example.flashcards_backend.dto.UploadResponse;
 
+import java.io.IOException;
 import java.io.InputStream;
 
 public interface CsvUploadService {
-    UploadResponse uploadCsv(InputStream csvStream) throws Exception;
+    UploadResponse uploadCsv(InputStream csvStream) throws IOException;
 }
