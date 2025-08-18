83dd414335b98a37b42b7687ef3935b9637eecd2
lucian <locamos@gmail.com>
2025-08-18 20:56:47 +0100

    FC-64 implements card hints

--- backend/src/main/java/com/example/flashcards_backend/dto/CardRequest.java
+++ backend/src/main/java/com/example/flashcards_backend/dto/CardRequest.java
@@ -2,26 +2,28 @@ package com.example.flashcards_backend.dto;
 
 import com.example.flashcards_backend.annotations.CardContent;
 import com.example.flashcards_backend.annotations.DeckName;
+import lombok.Builder;
 import lombok.NonNull;
+import org.hibernate.validator.constraints.Length;
 
 import java.util.Set;
 
+@Builder
 public record CardRequest(
     @CardContent String front,
     @CardContent String back,
+    @Length(max = 100) String hintFront,
+    @Length(max = 100) String hintBack,
     @NonNull
     Long subjectId,
     Set<@DeckName String> deckNames
 ) {
     public static CardRequest of(String front, String back, Long subjectId) {
-        return new CardRequest(front, back, subjectId, null);
+        return new CardRequest(front, back, null, null, subjectId, null);
     }
 
     public static CardRequest of(String front, String back, Long subjectId, String... deckNames) {
-        return new CardRequest(front, back, subjectId, Set.of(deckNames));
+        return new CardRequest(front, back,null, null, subjectId, Set.of(deckNames));
     }
 
-    public static CardRequest of(String front, String back, Long subjectId, Set<String> deckNames) {
-        return new CardRequest(front, back, subjectId, deckNames);
-    }
 }
