bf4a32e935c570144ccf29c0a449a830ee1a2e9f
lucian <locamos@gmail.com>
2025-08-22 20:14:12 +0100

    FC-99 implements UserStatsService

--- backend/src/main/java/com/example/flashcards_backend/service/UserStatsService.java
+++ backend/src/main/java/com/example/flashcards_backend/service/UserStatsService.java
@@ -2,23 +2,31 @@ package com.example.flashcards_backend.service;
 
 import com.example.flashcards_backend.dto.CardResponse;
 import com.example.flashcards_backend.dto.UserStatsResponse;
+import com.example.flashcards_backend.model.Card;
+import com.example.flashcards_backend.repository.CardRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
 
+@Service
+@RequiredArgsConstructor
 public class UserStatsService {
+
+    private final CardRepository cardRepository;
+
+    @Transactional(readOnly = true)
     public UserStatsResponse getForUserId(UUID userId) {
-        Long totalCards = 0L;
-        CardResponse mostViewedCard = null;
-        CardResponse hardestCard = null;
 
-
-
+        Long totalCards = cardRepository.countByUserId(userId);
+        Card mostViewedCard = cardRepository.findMostViewedByUserId(userId);
+        Card hardestCard = cardRepository.findHardestByUserId(userId);
 
         return UserStatsResponse.builder()
                 .totalCards(totalCards)
-                .hardestCard(hardestCard)
-                .mostViewedCard(mostViewedCard)
+                .hardestCard(CardResponse.fromEntity(hardestCard))
+                .mostViewedCard(CardResponse.fromEntity(mostViewedCard))
                 .build();
-
     }
 }
---
+++ backend/src/test/java/com/example/flashcards_backend/service/UserStatsServiceTest.java
@@ -0,0 +1,80 @@
+package com.example.flashcards_backend.service;
+
+import com.example.flashcards_backend.dto.CardResponse;
+import com.example.flashcards_backend.dto.UserStatsResponse;
+import com.example.flashcards_backend.model.Card;
+import com.example.flashcards_backend.model.Subject;
+import com.example.flashcards_backend.model.User;
+import com.example.flashcards_backend.repository.CardRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.UUID;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.mockito.Mockito.when;
+
+
+@ExtendWith(SpringExtension.class)
+class UserStatsServiceTest {
+
+    static final UUID USER_ID = UUID.fromString("00000000-0000-0000-0000-000000000000");
+
+    UserStatsService service;
+    @Mock
+    CardRepository cardRepository;
+
+    UserStatsResponse userStatsResponse;
+    Card hardestCard;
+    Card mostViewedCard;
+    User user;
+    Subject subject;
+
+    @BeforeEach
+    void setUp() {
+        service = new UserStatsService(cardRepository);
+
+        subject = Subject.builder().id(1L).name("Subject 1").build();
+        user = User.builder().id(USER_ID).build();
+        hardestCard = Card.builder()
+                .user(user)
+                .id(1L)
+                .front("Front 1")
+                .back("Back 1")
+                .subject(subject)
+                .build();
+        mostViewedCard = Card.builder()
+                .user(user)
+                .id(2L)
+                .front("Front 2")
+                .back("Back 2")
+                .subject(subject)
+                .build();
+
+        userStatsResponse = UserStatsResponse.builder()
+                .hardestCard(CardResponse.fromEntity(hardestCard))
+                .mostViewedCard(CardResponse.fromEntity(mostViewedCard))
+                .totalCards(100L)
+                .build();
+    }
+
+    @Test
+    void testGetForUserId_returnsUserStatsResponse() {
+        when(cardRepository.countByUserId(USER_ID)).thenReturn(100L);
+        when(cardRepository.findHardestByUserId(USER_ID)).thenReturn(hardestCard);
+        when(cardRepository.findMostViewedByUserId(USER_ID)).thenReturn(mostViewedCard);
+
+        UserStatsResponse response = service.getForUserId(USER_ID);
+        assertThat(response).isEqualTo(userStatsResponse);
+    }
+
+    @Test
+    void testGetForUserId_throwsExceptionIfUserNotFound() {
+        when(cardRepository.countByUserId(USER_ID)).thenThrow(IllegalArgumentException.class);
+        assertThatThrownBy(() -> service.getForUserId(USER_ID)).isInstanceOf(IllegalArgumentException.class);
+    }
+}
