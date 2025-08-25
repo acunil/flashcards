e88f0a3ad5f5f0d43aaa104297516c32fb468070
Lizzie <69165206+lizziedev@users.noreply.github.com>
2025-08-25 08:46:24 +0100

    FC-18 Create user stats hook and type and fix name of useCards function (#99)

---
+++ frontend/src/hooks/userStats/useUserStats.ts
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import { API_URL } from "../urls";
+import type { UserStats } from "../../types/user-stats";
+
+const useUserStats = (userId: string) => {
+  const [userStats, setUserStats] = useState<UserStats | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  const getUserStats = async () => {
+    try {
+      setLoading(true);
+      setError(null);
+
+      const response = await fetch(`${API_URL}/user-stats?userId=${userId}`);
+      if (!response.ok) {
+        throw new Error("Failed to fetch user stats");
+      }
+
+      const data: UserStats = await response.json();
+      setUserStats(data);
+    } catch (err: unknown) {
+      setError(err instanceof Error ? err.message : "Unknown error occurred");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { userStats, loading, error, getUserStats };
+};
+
+export default useUserStats;
