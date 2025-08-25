cbecafd6b830afdd534f7db458af2082df92a317
lizziedev <lizziebg1@gmail.com>
2025-08-25 15:35:17 +0100

    FC-111 Use logged in user bearer token in useAllCards request

--- frontend/src/hooks/cards/useAllCards.ts
+++ frontend/src/hooks/cards/useAllCards.ts
@@ -1,31 +1,45 @@
 import { useEffect, useState, useCallback } from "react";
 import type { Card } from "../../types/card";
 import { API_URL } from "../urls";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const useAllCards = (subjectId: number | null) => {
   const [cards, setCards] = useState<Card[]>([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
 
-  // Memoized fetch function
-  const fetchCards = useCallback(async (subjectIdParam: number) => {
-    setLoading(true);
-    setError(null);
+  const { getAccessTokenSilently } = useAuth0();
 
-    try {
-      const response = await fetch(
-        `${API_URL}/cards?subjectId=${subjectIdParam}`
-      );
-      if (!response.ok) throw new Error("Failed to fetch cards");
-      const data: Card[] = await response.json();
-      setCards(data);
-    } catch (err: unknown) {
-      if (err instanceof Error) setError(err.message);
-      else setError("Unknown error occurred");
-    } finally {
-      setLoading(false);
-    }
-  }, []);
+  const fetchCards = useCallback(
+    async (subjectIdParam: number) => {
+      setLoading(true);
+      setError(null);
+
+      try {
+        const token = await getAccessTokenSilently();
+        console.log(token);
+
+        console.log(token);
+        const response = await fetch(
+          `${API_URL}/cards?subjectId=${subjectIdParam}`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        if (!response.ok) throw new Error("Failed to fetch cards");
+        const data: Card[] = await response.json();
+        setCards(data);
+      } catch (err: unknown) {
+        if (err instanceof Error) setError(err.message);
+        else setError("Unknown error occurred");
+      } finally {
+        setLoading(false);
+      }
+    },
+    [getAccessTokenSilently]
+  );
 
   // Re-fetch cards whenever the subjectId changes
   useEffect(() => {
