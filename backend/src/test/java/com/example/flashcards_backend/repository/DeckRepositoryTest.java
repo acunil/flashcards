d8c2fc4b1614eafaae9ed897fce59e04fbc4277a
Lucian Amos <57368744+acunil@users.noreply.github.com>
2025-08-25 08:09:22 +0100

    FC-127 uses h2 db for tests (#97)
    mvn install builds green

--- backend/src/test/java/com/example/flashcards_backend/repository/DeckRepositoryTest.java
+++ backend/src/test/java/com/example/flashcards_backend/repository/DeckRepositoryTest.java
@@ -4,12 +4,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace.NONE;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = NONE)
+@ActiveProfiles("test")
 class DeckRepositoryTest {
 
     @Autowired
