cb4ea6277ecb99b6defe5b24b90c1e854f6aeb73
lizziedev <lizziebg1@gmail.com>
2025-08-27 07:09:30 +0100

    FC-159 only show hint button if there is a hint

--- frontend/src/components/reviseButtons/index.tsx
+++ frontend/src/components/reviseButtons/index.tsx
@@ -1,29 +1,26 @@
 import { LightbulbFilament, Pencil } from "phosphor-react";
 
 interface ReviseButtonProps {
-  disableHint: boolean;
+  showHintButton: boolean;
   onEdit: () => void;
   onShowHint: () => void;
 }
 
 const ReviseButtons = ({
-  disableHint,
+  showHintButton,
   onEdit,
   onShowHint,
 }: ReviseButtonProps) => {
   return (
     <div className="flex flex-row justify-end py-2 px-4 w-sm gap-2 max-w-screen">
-      <button
-        onClick={disableHint ? undefined : onShowHint}
-        className={`p-2 border-black border-2 rounded 
-    ${
-      disableHint
-        ? "bg-gray-200"
-        : "bg-yellow-200 hover:bg-yellow-300 cursor-pointer"
-    }`}
-      >
-        <LightbulbFilament size={20} />
-      </button>
+      {showHintButton && (
+        <button
+          onClick={onShowHint}
+          className={`p-2 border-black border-2 rounded  bg-yellow-200 hover:bg-yellow-300 cursor-pointer`}
+        >
+          <LightbulbFilament size={20} />
+        </button>
+      )}
       <button
         onClick={onEdit}
         className="bg-blue-200 p-2 border-black border-2 rounded cursor-pointer hover:bg-sky-300"
