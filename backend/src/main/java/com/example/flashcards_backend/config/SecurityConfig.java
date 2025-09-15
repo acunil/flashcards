503dfafe1822f43a07262d63b1e677398eae3684
lizziedev <lizziebg1@gmail.com>
2025-09-15 19:56:18 +0100

    lambda

--- backend/src/main/java/com/example/flashcards_backend/config/SecurityConfig.java
+++ backend/src/main/java/com/example/flashcards_backend/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.example.flashcards_backend.security.CustomJwtAuthenticationConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -17,7 +18,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
                 .cors(cors -> cors.configurationSource(corsConfigurationSource())) // use CorsConfigurationSource
                 .authorizeHttpRequests(authz -> authz
                         // allow swagger + API docs without authentication
