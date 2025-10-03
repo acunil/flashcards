783931012a898d7de70d095cdd0a68905a03a50c
lucian <locamos@gmail.com>
2025-10-03 18:02:25 +0100

    FC-237 updates Card equals for sonar compliance
    removes unused tests

--- backend/src/main/java/com/example/flashcards_backend/model/Card.java
+++ backend/src/main/java/com/example/flashcards_backend/model/Card.java
@@ -122,6 +122,7 @@ public class Card {
         cardHistory.setCard(this);
     }
 
+    @SuppressWarnings("unused")
     public static class CardBuilder {
         private Long id;
         private String front;
@@ -159,15 +160,15 @@ public class Card {
     public final boolean equals(Object o) {
         if (this == o) return true;
         if (o == null) return false;
-        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
-        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (!(o instanceof Card other)) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy proxy ? proxy.getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy proxy ? proxy.getHibernateLazyInitializer().getPersistentClass() : this.getClass();
         if (thisEffectiveClass != oEffectiveClass) return false;
-        Card card = (Card) o;
-        return getId() != null && Objects.equals(getId(), card.getId());
+        return getId() != null && Objects.equals(getId(), other.getId());
     }
 
     @Override
     public final int hashCode() {
-        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+        return this instanceof HibernateProxy proxy ? proxy.getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
     }
 }
