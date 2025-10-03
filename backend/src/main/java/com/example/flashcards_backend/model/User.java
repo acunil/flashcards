33b6f8b7477589941e15787b2fa10dc59810d66a
lucian <locamos@gmail.com>
2025-10-03 18:20:35 +0100

    FC-238 removes cards and decks from User

--- backend/src/main/java/com/example/flashcards_backend/model/User.java
+++ backend/src/main/java/com/example/flashcards_backend/model/User.java
@@ -3,8 +3,6 @@ package com.example.flashcards_backend.model;
 import jakarta.persistence.*;
 import lombok.*;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
 
 @Entity
@@ -28,16 +26,4 @@ public class User {
 
   @Column(name = "auth0_id", length = 100, unique = true)
   private String auth0Id;
-
-  @Builder.Default
-  @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
-  private List<Card> cards = new ArrayList<>();
-
-  @Builder.Default
-  @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
-  private List<Deck> decks = new ArrayList<>();
-
-  @Builder.Default
-  @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
-  private List<CardHistory> cardHistories = new ArrayList<>();
 }
