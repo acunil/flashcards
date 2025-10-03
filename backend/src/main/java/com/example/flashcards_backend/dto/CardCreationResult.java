3c6e39c94ea9414825d5446306f65fe0279d344c
Lucian Amos <57368744+acunil@users.noreply.github.com>
2025-10-03 17:49:21 +0100

    FC-237 removes unused DTOs
    Signed-off-by: lucian <locamos@gmail.com>

--- backend/src/main/java/com/example/flashcards_backend/dto/CardCreationResult.java
+++
@@ -1,6 +0,0 @@
-package com.example.flashcards_backend.dto;
-
-import com.example.flashcards_backend.model.Card;
-import lombok.NonNull;
-
-public record CardCreationResult(@NonNull Card card, @NonNull Boolean alreadyExisted) {}
