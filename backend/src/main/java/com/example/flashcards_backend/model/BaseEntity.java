4ba8066483b3a4af4df9de4876d56382cf1946c9
lucian <locamos@gmail.com>
2025-10-03 17:57:21 +0100

    FC-237 updates BaseEntity for sonar compliance

--- backend/src/main/java/com/example/flashcards_backend/model/BaseEntity.java
+++ backend/src/main/java/com/example/flashcards_backend/model/BaseEntity.java
@@ -14,37 +14,38 @@ import java.util.Objects;
 @SuperBuilder
 public abstract class BaseEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Getter
-    @Setter
-    private Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Getter
+  @Setter
+  private Long id;
 
-    @Override
-    public final boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null) return false;
+  @Override
+  public final boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null) return false;
 
-        Class<?> oEffectiveClass =
-                o instanceof HibernateProxy
-                        ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass()
-                        : o.getClass();
+    if (!(o instanceof BaseEntity other)) return false;
 
-        Class<?> thisEffectiveClass =
-                this instanceof HibernateProxy
-                        ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass()
-                        : this.getClass();
+    Class<?> thisEffectiveClass =
+        (this instanceof HibernateProxy proxy)
+            ? proxy.getHibernateLazyInitializer().getPersistentClass()
+            : this.getClass();
 
-        if (thisEffectiveClass != oEffectiveClass) return false;
+    Class<?> otherEffectiveClass =
+        (o instanceof HibernateProxy proxy)
+            ? proxy.getHibernateLazyInitializer().getPersistentClass()
+            : o.getClass();
 
-        BaseEntity that = (BaseEntity) o;
-        return getId() != null && Objects.equals(getId(), that.getId());
-    }
+    if (!thisEffectiveClass.equals(otherEffectiveClass)) return false;
 
-    @Override
-    public final int hashCode() {
-        return this instanceof HibernateProxy
-                ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode()
-                : getClass().hashCode();
-    }
+    return getId() != null && Objects.equals(getId(), other.getId());
+  }
+
+  @Override
+  public final int hashCode() {
+    return (this instanceof HibernateProxy proxy)
+        ? proxy.getHibernateLazyInitializer().getPersistentClass().hashCode()
+        : getClass().hashCode();
+  }
 }
